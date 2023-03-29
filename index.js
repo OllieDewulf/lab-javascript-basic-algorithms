820cbe37b9c9647e45061264ab3cfb11790012a9
Olivier Dewulf <olivierdewulf@Oliviers-MacBook-Pro.local>
2023-03-29 16:25:16 +0200

    Day 3 Exercise

--- index.js
+++ index.js
@@ -1,7 +1,46 @@
 // Iteration 1: Names and Input
-
+const hacker1 = "Olivier"
+console.log ("The driver's name is " + hacker1)
+const hacker2 = "Sarah"
+console.log ("The navigator's name is " + hacker2)
 
 // Iteration 2: Conditionals
+if (hacker1.length>hacker2.length) 
+{
+  console.log (`The driver has the longest name, it has ${hacker1.length} characters.`)
+}
 
+else if (hacker1.length<hacker2.length)
+{
+  console.log (`It seems that the navigator has the longest name, it has ${hacker1.length} characters.`)
+}
+
+else {
+  console.log (`Wow, you both have equally long names, ${hacker2.length} characters!`)
+     }
 
 // Iteration 3: Loops
+let name1 = " "
+for (let i =0; i<hacker1.length; i++){
+  name1 += hacker1[i] + " "
+}
+console.log (name1.toUpperCase())
+
+let name2 = " "
+for (let j =hacker2.length-1; j>=0; j--){
+  name2 += hacker2[j]
+}
+console.log (name2)
+
+
+if (name1>name2)
+{
+  console.log (`The driver's name goes first.`)
+}
+else if (name1<name2)
+{
+  console.log (`Yo, the navigator goes first, definitely.`)
+}
+else { 
+  console.log (`What?! You both have the same name?`)
+} 
